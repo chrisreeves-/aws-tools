e1c1d7068d289aa888137567e43fdb45f06b8f9f
Christopher Reeves <chris@reeves.net.nz>
2022-07-08 15:46:38 +1000

    .

--- upload-ecrdocker/upload-ecrdocker.sh
+++ upload-ecrdocker/upload-ecrdocker.sh
@@ -6,7 +6,7 @@ ecrrepo=""
 docker tag $imagename $awsid.dkr.ecr.$awsregion.amazonaws.com/$ecrrepo
 aws ecr get-login-password | docker login --username AWS --password-stdin $awsid.dkr.ecr.$awsregion.amazonaws.com
 docker build -t $imagename .
-docker tag $imagename $awsid.dkr.ecr.$awsregion.amazonaws.com/$ecrrepo:latest
+docker tag $imagename $awsid.dkr.ecr.$awsregion.amazonaws.com/$ecrrepo
 docker push $awsid.dkr.ecr.$awsregion.amazonaws.com/$ecrrepo
 
 # Test Image
